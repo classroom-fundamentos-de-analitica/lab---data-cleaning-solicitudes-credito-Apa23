ff356d20137af1a143bbfa4187b2c568a7ae0789
Andres Felipe Aparicio Mestre <anapariciom@unal.edu.co>
2022-11-10 16:27:08 -0500

    solucion taller

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,35 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+import numpy as np
+import re
+from datetime import datetime
 
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
 
+    df = df.drop(['Unnamed: 0'], axis=1)
+    for i in df:
+      if(i=="fecha_de_beneficio"):
+        df[i] = df[i].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+        continue
+      elif(i=="monto_del_credito"):
+        df[i] = df[i].str.replace('$ ', '', regex=False)
+        df[i] = df[i].str.replace(',', '', regex=False)
+        df[i] = df[i].str.replace('.00', '', regex=False)
+        df[i] = df[i].astype(int)
+        continue
+        
+      elif(df[i].dtype!="int64" and df[i].dtype!="float64"):
+        df[i] = df[i].str.lower()
+        df[i] = df[i].str.replace('-', ' ', regex=False)
+        df[i] = df[i].str.replace('_', ' ', regex=False)
+        
+    df.drop_duplicates(inplace=True)
     return df
+
+clean_data()
